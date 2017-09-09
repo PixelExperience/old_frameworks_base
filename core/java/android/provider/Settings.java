141e34e019feffe0e552a5fbb16ca5429d494966
maxwen <max.weninger@gmail.com>
2017-09-09 22:32:52 +0800

    policy: readd volume button wake
    Change-Id: If360f6b3a57a2111bea0409f78dfb52c1d2535ec
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4078,6 +4078,12 @@ public final class Settings {
         public static final String VOLUME_BUTTON_MUSIC_CONTROL = "volume_button_music_control";
 
         /**
+         * Whether to wake the screen with the volume keys, the value is boolean.
+         * @hide
+         */
+        public static final String VOLUME_WAKE_SCREEN = "volume_wake_screen";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
