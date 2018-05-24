Team-Xtreme/frameworks_base

HW Keys customization support [2/4]
Based on DUI

jhenrique09 edits: make it DUI independent

Original commit:
Squashed commit of the following:

Manually bring in

DUI: Migrate to API/Permission model for Editor [1/5]
https://github.com/bigrushdog/android_frameworks_base/commit/20681de6a18ef2e1851d66c095658a4f1479c054

commit d3dd2b5df493989f3bbb479a5734df3a84d9598a
Author: bigrushdog <randall.rushing@gmail.com>
Date:   Fri Aug 11 18:15:22 2017 -0700

    DUtils: Migrate from broadcasts to API for some action handling [1/2]

    Protected broadcasts for ActionHandler actions will become problematic
    in the future. Currently, broadcasts are sent from SystemUI as well as
    system server (hwkeys). This may also change in the future. So provide
    a general API solution with properly secured public methods.

    Change-Id: I90ee0dd5fe75545d50ca77dd0fecba86ab23a09e

commit 6a528e1261649a404ee70ea74dda1551f53a30e1
Author: bigrushdog <randall.rushing@gmail.com>
Date:   Wed Aug 2 16:10:54 2017 -0700

    DUI: Left in landscape navbar API [1/3]

    Window manager will let SystemUI know when navbar
    enters or exits the "left in landscape" position.
    We tried to avoid this using orientation listeners
    but that approach proved unreliable leaving Pulse
    in bizarre states when navbar was left in landscape

    Change-Id: Ie67b905755cd20219f08e76232a10fde50ec9b85

commit 7e8cb8b0cfa47d0b1e1105f97905167efafac851
Author: Randall Rushing <randall.rushing@gmail.com>
Date:   Thu Oct 29 05:02:04 2015 -0700

    Pulse: forward port visualizer lock api

    Change-Id: I13c7bbc8f4dc79fa41f11cbab25c1cdd83aed2af

commit 593d60fcea0a2edfebb1ed9ebc21ab69238cb7e7
Author: Randall Rushing <randall.rushing@gmail.com>
Date:   Thu Jan 7 02:41:02 2016 -0800

    statusbar: add screenPinningStateChanged(boolean enabled) api and pipe to navigation bar

    Let activity stack notify statusbar when task locking state changes. This is needed for
    alternate task managers (OmniSwitch,

packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -84,6 +84,8 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_SET_TOP_APP_HIDES_STATUS_BAR  = 37 << MSG_SHIFT;
     private static final int MSG_TOGGLE_NAVIGATION_BAR         = 38 << MSG_SHIFT;
     private static final int MSG_TOGGLE_CAMERA_FLASH           = 39 << MSG_SHIFT;
+    private static final int MSG_SCREEN_PINNING_STATE_CHANGED  = 40 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_FLASHLIGHT             = 41 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -147,6 +149,9 @@ public class CommandQueue extends IStatusBar.Stub {
 
         default void toggleNavigationBar(boolean enable) { }
         default void toggleCameraFlash() { }
+
+        default void screenPinningStateChanged(boolean enabled) {}
+        default void toggleFlashlight() {}
     }
 
     @VisibleForTesting
@@ -160,6 +165,21 @@ public class CommandQueue extends IStatusBar.Stub {
 
     public void removeCallbacks(Callbacks callbacks) {
         mCallbacks.remove(callbacks);
+    }
+
+    public void toggleFlashlight() {
+        synchronized (mLock) {
+            mHandler.removeMessages(MSG_TOGGLE_FLASHLIGHT);
+            mHandler.sendEmptyMessage(MSG_TOGGLE_FLASHLIGHT);
+        }
+    }
+
+    public void screenPinningStateChanged(boolean enabled) {
+        synchronized (mLock) {
+            mHandler.removeMessages(MSG_SCREEN_PINNING_STATE_CHANGED);
+            mHandler.obtainMessage(MSG_SCREEN_PINNING_STATE_CHANGED,
+                    enabled ? 1 : 0, 0, null).sendToTarget();
+        }
     }
 
     public void setIcon(String slot, StatusBarIcon icon) {
@@ -681,6 +701,16 @@ public class CommandQueue extends IStatusBar.Stub {
                 case MSG_TOGGLE_CAMERA_FLASH:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).toggleCameraFlash();
+                    }
+                    break;
+                case MSG_SCREEN_PINNING_STATE_CHANGED:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).screenPinningStateChanged(msg.arg1 != 0);
+                    }
+                    break;
+                case MSG_TOGGLE_FLASHLIGHT:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).toggleFlashlight();
                     }
                     break;
             }

PixelExperience/old_frameworks_base

Change-Id: I436ab30b7f44dafc89f8369ee1225ea177d29ac9

packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -83,6 +83,8 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_SHOW_SHUTDOWN_UI              = 36 << MSG_SHIFT;
     private static final int MSG_SET_TOP_APP_HIDES_STATUS_BAR  = 37 << MSG_SHIFT;
     private static final int MSG_TOGGLE_NAVIGATION_BAR         = 38 << MSG_SHIFT;
+    private static final int MSG_SCREEN_PINNING_STATE_CHANGED  = 39 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_FLASHLIGHT             = 40 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -144,6 +146,9 @@ public class CommandQueue extends IStatusBar.Stub {
         default void handleShowGlobalActionsMenu() { }
         default void handleShowShutdownUi(boolean isReboot, String reason) { }
         default void toggleNavigationBar(boolean enable) { }
+
+        default void screenPinningStateChanged(boolean enabled) {}
+        default void toggleFlashlight() {}
     }
 
     @VisibleForTesting
@@ -157,6 +162,21 @@ public class CommandQueue extends IStatusBar.Stub {
 
     public void removeCallbacks(Callbacks callbacks) {
         mCallbacks.remove(callbacks);
+    }
+
+    public void toggleFlashlight() {
+        synchronized (mLock) {
+            mHandler.removeMessages(MSG_TOGGLE_FLASHLIGHT);
+            mHandler.sendEmptyMessage(MSG_TOGGLE_FLASHLIGHT);
+        }
+    }
+
+    public void screenPinningStateChanged(boolean enabled) {
+        synchronized (mLock) {
+            mHandler.removeMessages(MSG_SCREEN_PINNING_STATE_CHANGED);
+            mHandler.obtainMessage(MSG_SCREEN_PINNING_STATE_CHANGED,
+                    enabled ? 1 : 0, 0, null).sendToTarget();
+        }
     }
 
     public void setIcon(String slot, StatusBarIcon icon) {
@@ -666,6 +686,16 @@ public class CommandQueue extends IStatusBar.Stub {
                 case MSG_TOGGLE_NAVIGATION_BAR:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).toggleNavigationBar(msg.arg1 != 0);
+                    }
+                    break;
+                case MSG_SCREEN_PINNING_STATE_CHANGED:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).screenPinningStateChanged(msg.arg1 != 0);
+                    }
+                    break;
+                case MSG_TOGGLE_FLASHLIGHT:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).toggleFlashlight();
                     }
                     break;
             }