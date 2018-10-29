e61d47f25655fbc1a01c21568cd78a075e8a9f53
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-10-29 17:32:21 -0300

    WIP: Implement weather on lockscreen like Pixels
    This is a squashed version of Omni and Havoc implementation for keyguard on lockscreen
    
    @jhenrique09 edits:
    
    * Cleanup code
    * Adapt to our weather provider
    
    Squashed commits:
    
    commit 9eec25ee055aede3f3e989ed5884c4b6607a4a1a
    Author: maxwen <max.weninger@gmail.com>
    Date:   Thu Aug 30 12:25:45 2018 +0200
    
        [1/3] base: add back weather parts
    
        Change-Id: I7568be52af6ea966c8304f47b0601740b51d6c55
    
    commit f3afd3479be63173217e7886cd569b0eeccd6a19
    Author: maxwen <max.weninger@gmail.com>
    Date:   Thu Aug 30 22:21:34 2018 +0200
    
        [1/3] base: SystemUI: keyguard weather view
    
        Change-Id: Idbedea20d5487a42849c225e3f500aa1f0282c6c
    
    commit dff718fd34643eeb92fa460fd6fb0ab587a81837
    Author: maxwen <max.weninger@gmail.com>
    Date:   Sun Sep 2 20:28:56 2018 +0200
    
        base: SystemUI: hide keyguard weather when pulsing
    
        Change-Id: Ied8d34fbdfcfbd0deda47a783cacebaaa58bf1e4
    
    commit 8b88898660bbb9f5517656815a917a59cad950fa
    Author: SKULSHADY <anushekprasal@gmail.com>
    Date:   Sun Sep 16 15:50:32 2018 +0530
    
        Keyguard weather minimal layout
    
        Remove the city view to keep keyguard clean and not cluttered.
    
    commit 9ac197b7cf652de1800ec4fd4dfa83e9176e9b65
    Author: SKULSHADY <anushekprasal@gmail.com>
    Date:   Thu Sep 20 02:57:41 2018 +0530
    
        Keyguard Weather improvements
    
        Move weather to date view. This matches the weather layout seen on Android P on Pixel phones. Also enable it by default.
    
    commit 93c6f36aa7b3b9f983233670c5f9a2966d262acf
    Author: maxwen <max.weninger@gmail.com>
    Date:   Thu Sep 27 13:26:05 2018 +0200
    
        base: SystemUI: fix keyguard weather color update
    
        Change-Id: I9bb9861d824b44fb2d1e91dd61e731838a0c84ac
    
    Change-Id: I05c3d109edb9c38ad11e14dfc17f4dd06a9bcb1d
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>

---
+++ packages/SystemUI/src/com/android/keyguard/weather/WeatherClient.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright (C) 2018 The OmniROM Project
+ *                    The PixelExperience Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.android.keyguard.weather;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WeatherClient {
+
+    private static final String TAG = "SystemUI:WeatherClient";
+    private static final boolean DEBUG = true;
+    private static final String SERVICE_PACKAGE = "org.pixelexperience.weather.client";
+    private static final Uri WEATHER_URI = Uri.parse("content://org.pixelexperience.weather.client.provider/weather");
+
+    private static final String COLUMN_STATUS = "status";
+    private static final String COLUMN_CONDITIONS = "conditions";
+    private static final String COLUMN_TEMPERATURE_METRIC = "temperatureMetric";
+    private static final String COLUMN_TEMPERATURE_IMPERIAL = "temperatureImperial";
+    private static final String[] PROJECTION_DEFAULT_WEATHER = new String[]{
+            COLUMN_STATUS,
+            COLUMN_CONDITIONS,
+            COLUMN_TEMPERATURE_METRIC,
+            COLUMN_TEMPERATURE_IMPERIAL
+    };
+
+    private Context mContext;
+    private List<WeatherObserver> mObserver;
+
+    public WeatherClient(Context context) {
+        mContext = context;
+        mObserver = new ArrayList<>();
+        new WeatherContentObserver(new Handler()).observe();
+    }
+
+    public static boolean isAvailable(Context context) {
+        final PackageManager pm = context.getPackageManager();
+        try {
+            pm.getPackageInfo(SERVICE_PACKAGE, PackageManager.GET_ACTIVITIES);
+            int enabled = pm.getApplicationEnabledSetting(SERVICE_PACKAGE);
+            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+                    enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    WeatherInfo getWeatherData() {
+        if (!isAvailable(mContext)) {
+            return null;
+        }
+        WeatherInfo weatherInfo = new WeatherInfo();
+        Cursor c = mContext.getContentResolver().query(WEATHER_URI, PROJECTION_DEFAULT_WEATHER,
+                null, null, null);
+        if (c != null) {
+            try {
+                int count = c.getCount();
+                if (count > 0) {
+                    for (int i = 0; i < count; i++) {
+                        c.moveToPosition(i);
+                        if (i == 0) {
+                            weatherInfo.status = c.getInt(0);
+                            weatherInfo.conditions = c.getString(1);
+                            weatherInfo.temperatureMetric = c.getInt(2);
+                            weatherInfo.temperatureImperial = c.getInt(3);
+                        }
+                    }
+                }
+            } finally {
+                c.close();
+            }
+        }
+        if (DEBUG) Log.d(TAG, weatherInfo.toString());
+        return weatherInfo;
+    }
+
+    public void addObserver(WeatherObserver observer) {
+        mObserver.add(observer);
+    }
+
+    public void removeObserver(WeatherObserver observer) {
+        mObserver.remove(observer);
+    }
+
+    public interface WeatherObserver {
+        void onWeatherUpdated(WeatherInfo info);
+    }
+
+    private class WeatherContentObserver extends ContentObserver {
+        WeatherContentObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            mContext.getContentResolver().registerContentObserver(WEATHER_URI, false, this, UserHandle.USER_ALL);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            WeatherInfo info = getWeatherData();
+            for (WeatherObserver observer : mObserver) {
+                observer.onWeatherUpdated(info);
+            }
+        }
+    }
+
+    public class WeatherInfo {
+
+        int status = 1;
+        String conditions = "";
+        int temperatureMetric = 0;
+        int temperatureImperial = 0;
+
+        WeatherInfo() {
+        }
+
+        int getTemperature(boolean metric) {
+            return metric ? this.temperatureMetric : this.temperatureImperial;
+        }
+
+        int getStatus() {
+            return this.status;
+        }
+
+        String getConditions() {
+            return this.conditions;
+        }
+
+        @NonNull
+        @Override
+        public String toString() {
+            return "WeatherInfo: " +
+                    "status=" + getStatus() + "," +
+                    "conditions=" + getConditions() + "," +
+                    "temperatureMetric=" + getTemperature(true) + "," +
+                    "temperatureImperial=" + getTemperature(false);
+        }
+    }
+}
