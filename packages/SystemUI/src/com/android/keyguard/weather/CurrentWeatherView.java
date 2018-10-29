4091114290c2a3261e51b0af415a9ef476253a45
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-10-29 17:32:21 -0300

    WIP: Implement weather on lockscreen like Pixels
    This is a squashed version of Omni and Havoc implementation for keyguard on lockscreen
    
    @jhenrique09 edits:
    
    * Cleanup code
    * Adapt to our weather provider
    
    Squashed commits:
    
    commit 9eec25ee055aede3f3e989ed5884c4b6607a4a1a
    Author: maxwen <max.weninger@gmail.com>
    Date:   Thu Aug 30 12:25:45 2018 +0200
    
        [1/3] base: add back weather parts
    
        Change-Id: I7568be52af6ea966c8304f47b0601740b51d6c55
    
    commit f3afd3479be63173217e7886cd569b0eeccd6a19
    Author: maxwen <max.weninger@gmail.com>
    Date:   Thu Aug 30 22:21:34 2018 +0200
    
        [1/3] base: SystemUI: keyguard weather view
    
        Change-Id: Idbedea20d5487a42849c225e3f500aa1f0282c6c
    
    commit dff718fd34643eeb92fa460fd6fb0ab587a81837
    Author: maxwen <max.weninger@gmail.com>
    Date:   Sun Sep 2 20:28:56 2018 +0200
    
        base: SystemUI: hide keyguard weather when pulsing
    
        Change-Id: Ied8d34fbdfcfbd0deda47a783cacebaaa58bf1e4
    
    commit 8b88898660bbb9f5517656815a917a59cad950fa
    Author: SKULSHADY <anushekprasal@gmail.com>
    Date:   Sun Sep 16 15:50:32 2018 +0530
    
        Keyguard weather minimal layout
    
        Remove the city view to keep keyguard clean and not cluttered.
    
    commit 9ac197b7cf652de1800ec4fd4dfa83e9176e9b65
    Author: SKULSHADY <anushekprasal@gmail.com>
    Date:   Thu Sep 20 02:57:41 2018 +0530
    
        Keyguard Weather improvements
    
        Move weather to date view. This matches the weather layout seen on Android P on Pixel phones. Also enable it by default.
    
    commit 93c6f36aa7b3b9f983233670c5f9a2966d262acf
    Author: maxwen <max.weninger@gmail.com>
    Date:   Thu Sep 27 13:26:05 2018 +0200
    
        base: SystemUI: fix keyguard weather color update
    
        Change-Id: I9bb9861d824b44fb2d1e91dd61e731838a0c84ac
    
    Change-Id: I05c3d109edb9c38ad11e14dfc17f4dd06a9bcb1d
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>

---
+++ packages/SystemUI/src/com/android/keyguard/weather/CurrentWeatherView.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright (C) 2018 The OmniROM Project
+ *                    The PixelExperience Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.android.keyguard.weather;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.VectorDrawable;
+import android.support.v4.graphics.ColorUtils;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.settingslib.Utils;
+
+public class CurrentWeatherView extends FrameLayout implements WeatherClient.WeatherObserver {
+
+    static final String TAG = "SystemUI:CurrentWeatherView";
+    static final boolean DEBUG = true;
+
+    private ImageView mCurrentImage;
+    private WeatherClient mWeatherClient;
+    private TextView mRightText;
+    private int mTextColor;
+    private float mDarkAmount;
+
+    public CurrentWeatherView(Context context) {
+        this(context, null);
+    }
+
+    public CurrentWeatherView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CurrentWeatherView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        mCurrentImage = findViewById(R.id.current_image);
+        mRightText = findViewById(R.id.right_text);
+        mTextColor = mRightText.getCurrentTextColor();
+        setVisibility(View.GONE);
+        if (WeatherClient.isAvailable(getContext())) {
+            mWeatherClient = new WeatherClient(getContext());
+            mWeatherClient.addObserver(this);
+            queryAndUpdateWeather(null);
+        }
+    }
+
+    private void updateWeatherData(WeatherClient.WeatherInfo weatherData) {
+        if (DEBUG) Log.d(TAG, "updateWeatherData");
+
+        if (!WeatherClient.isAvailable(getContext()) || weatherData == null || weatherData.getStatus() != WeatherClient.WEATHER_UPDATE_SUCCESS) {
+            setVisibility(View.GONE);
+            return;
+        }
+
+        boolean useMetric = true; // TODO: Add entry on Settings > System > Language and input, to switch between metric and imperial
+        int temperatureMetric = weatherData.getTemperature(true);
+        int temperatureImperial = weatherData.getTemperature(false);
+        String temperatureText = useMetric ? 
+                                 Integer.toString(temperatureMetric) + " °C" : 
+                                 Integer.toString(temperatureImperial) + " °F";
+
+/*
+        String conditions = weatherData.getConditions();
+        Drawable d = getWeatherConditionImage(conditions);
+        d = d.mutate();
+        updateTint(d);
+        mCurrentImage.setImageDrawable(d);*/
+
+        mRightText.setText(temperatureText);
+        setVisibility(View.VISIBLE);
+    }
+
+    private int getTintColor() {
+        return Utils.getColorAttr(mContext, R.attr.wallpaperTextColor);
+    }
+
+    @Override
+    public void onWeatherUpdated(WeatherClient.WeatherInfo weatherData) {
+        if (DEBUG) Log.d(TAG, "weatherUpdated");
+        queryAndUpdateWeather(weatherData);
+    }
+
+    private void queryAndUpdateWeather(WeatherClient.WeatherInfo weatherData) {
+        if (mWeatherClient != null) {
+            if (DEBUG) Log.d(TAG, "queryAndUpdateWeather");
+            if (weatherData == null) {
+                weatherData = mWeatherClient.getWeatherData();
+            }
+            updateWeatherData(weatherData);
+        }
+    }
+
+    public void blendARGB(float darkAmount) {
+        mDarkAmount = darkAmount;
+        mRightText.setTextColor(ColorUtils.blendARGB(mTextColor, Color.WHITE, darkAmount));
+
+        if (mWeatherClient != null) {
+            // update image with correct tint
+            updateWeatherData(mWeatherClient.getWeatherData());
+        }
+    }
+
+    private void updateTint(Drawable d) {
+        if (mDarkAmount == 1) {
+            mCurrentImage.setImageTintList(ColorStateList.valueOf(Color.WHITE));
+        } else {
+            mCurrentImage.setImageTintList((d instanceof VectorDrawable) ? ColorStateList.valueOf(getTintColor()) : null);
+        }
+    }
+
+    public void onDensityOrFontScaleChanged() {
+        mRightText.setTextSize(TypedValue.COMPLEX_UNIT_PX,
+                getResources().getDimensionPixelSize(R.dimen.widget_label_font_size));
+        mCurrentImage.getLayoutParams().height =
+                getResources().getDimensionPixelSize(R.dimen.current_weather_image_size);
+        mCurrentImage.getLayoutParams().width =
+                getResources().getDimensionPixelSize(R.dimen.current_weather_image_size);
+    }
+}
