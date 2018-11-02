34d0fac183d166f2fcddb4a7e7303e3e752e8fdb
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-11-02 14:25:52 -0300

    AmbientPlay: Rework
    * Cleanup settings
      - Move to Settings.System
      - Remove AMBIENT_RECOGNITION_INTERVAL
    
    * Make notifications optional
    
    * Escape html entities
    
    * Move all classes to systemui
    
    Change-Id: Ief48416a85ee07c0858569adbda20930d09c3c32

--- packages/SystemUI/src/com/android/systemui/ambient/play/RecognitionObserver.java
+++ packages/SystemUI/src/com/android/systemui/ambient/play/RecognitionObserver.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2014 Fastboot Mobile, LLC.
  * Copyright (C) 2018 CypherOS
+ * Copyright (C) 2018 PixelExperience
  *
  * This program is free software; you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation; either version 3 of
@@ -13,14 +14,15 @@
  * You should have received a copy of the GNU General Public License along with this program;
  * if not, see <http://www.gnu.org/licenses>.
  */
+
 package com.android.systemui.ambient.play;
 
-import android.ambient.AmbientIndicationManager;
-import android.ambient.play.RecoginitionObserver;
-import android.ambient.play.WaveFormat;
 import android.content.Context;
+import android.media.AudioFormat;
 import android.media.AudioRecord;
-import android.os.SystemClock;
+import android.media.MediaRecorder;
+import android.os.Process;
+import android.provider.Settings;
 import android.util.Log;
 
 import java.io.IOException;
@@ -30,16 +32,81 @@ import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang.StringEscapeUtils;
+
 /**
  * Class helping audio fingerprinting for recognition
  */
-public class RecoginitionObserverFactory extends RecoginitionObserver {
+public class RecognitionObserver implements AmbientIndicationManagerCallback {
+
+    private static final String TAG = "RecognitionObserver";
+    private static final String USER_AGENT = "User-Agent: AppNumber=48000 APIVersion=2.1.0.0 DEV=Android UID=dkl109sas19s";
+    private static final String MIME_TYPE = "audio/wav";
+
+    private static final int SAMPLE_RATE = 11025;
+    private static final short BIT_DEPTH = 16;
+    private static final short CHANNELS = 1;
+
+    private byte[] mBuffer;
+    private AudioRecord mRecorder;
 
     private RecorderThread mRecThread;
-    boolean isRecording = false;
+    private boolean isRecording = false;
+    private AmbientIndicationManager mManager;
+    private boolean isRecognitionEnabled;
 
-    public RecoginitionObserverFactory(Context context) {
-        super(context);
+    RecognitionObserver(Context context, AmbientIndicationManager manager) {
+        this.mManager = manager;
+        manager.registerCallback(this);
+        int bufferSize = SAMPLE_RATE * 11 * 2;
+        mBuffer = new byte[bufferSize];
+        final int minBufSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT);
+        mRecorder = new AudioRecord(MediaRecorder.AudioSource.CAMCORDER, SAMPLE_RATE,
+                AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                minBufSize);
+    }
+
+    @Override
+    public void onRecognitionResult(Observable observed) {
+
+    }
+
+    @Override
+    public void onRecognitionNoResult() {
+
+    }
+
+    @Override
+    public void onRecognitionError() {
+
+    }
+
+    @Override
+    public void onSettingsChanged(String key, boolean newValue) {
+        if (key.contains(Settings.System.AMBIENT_RECOGNITION)){
+            isRecognitionEnabled = newValue;
+            if (!isRecognitionEnabled){
+                stopRecording();
+            }
+        }
+    }
+
+    /**
+     * Class storing fingerprinting results
+     */
+    public static class Observable {
+
+        String Artist;
+         String Album;
+         String Song;
+         String ArtworkUrl;
+
+        @Override
+        public String toString() {
+            return Artist + " - " + Song + " (" + Album + "); " + ArtworkUrl;
+        }
     }
 
     /**
@@ -50,7 +117,7 @@ public class RecoginitionObserverFactory extends RecoginitionObserver {
         private byte[] buffCopy = new byte[SAMPLE_RATE * 10 * 2];
 
         public void run() {
-            android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND);
+            Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
             Log.d(TAG, "Started reading recorder...");
 
             while (isRecording && mBuffer != null) {
@@ -70,13 +137,6 @@ public class RecoginitionObserverFactory extends RecoginitionObserver {
                         }
                     }
                 }
-
-                // if (read >= 0) {
-                //     if (mBufferIndex > 10) {
-                //         mManager.dispatchRecognitionAudio((float) computeAverageAmplitude(mBuffer, mBufferIndex - 10, 4));
-                //     }
-                //     tryMatchCurrentBuffer();
-                // }
             }
 
             tryMatchCurrentBuffer();
@@ -84,29 +144,15 @@ public class RecoginitionObserverFactory extends RecoginitionObserver {
             Log.d(TAG, "Broke out of recording loop, mResultGiven=" + mResultGiven);
         }
 
-        private double computeAverageAmplitude(byte[] buffer, int startSample, int numSamples) {
-            // Assuming 16 bits depth
-            double sum = 0.0f;
-            for (int i = 0; i < numSamples; ++i) {
-                sum += computeAmplitude(buffer, startSample + i * 2);
+        private void tryMatchCurrentBuffer() {
+            if (!isRecognitionEnabled){
+                stopRecording();
+                return;
             }
-
-            return sum / ((double) numSamples);
-        }
-
-        private double computeAmplitude(byte[] buffer, int sample) {
-            // Assuming 16 bits depth
-            int amplitude = (buffer[sample] & 0xff) << 8 | buffer[sample + 1];
-            // decibel: return 20.0 * Math.log10((double)Math.abs(amplitude) / 32768.0);
-            return amplitude / 65536.0;
-        }
-
-        public void tryMatchCurrentBuffer() {
-            if (!mRecognitionEnabled) return;
             if (!isRecording) {
                 new Thread() {
                     public void run() {
-                        android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND);
+                        Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND);
                         // Allow only one upload call at a time
                         String output_xml = sendAudioData(buffCopy, buffCopy.length);
                         parseXmlResult(output_xml);
@@ -125,8 +171,8 @@ public class RecoginitionObserverFactory extends RecoginitionObserver {
                 conn.addRequestProperty("User-Agent", USER_AGENT);
                 conn.addRequestProperty("Content-Type", MIME_TYPE);
                 conn.setDoOutput(true);
-                conn.setConnectTimeout(4000);
-                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(20000);
+                conn.setReadTimeout(20000);
 
                 // Write the WAVE audio header, then the PCM data
                 Log.d(TAG, "Sending mic data, " + length + " bytes...");
@@ -165,9 +211,9 @@ public class RecoginitionObserverFactory extends RecoginitionObserver {
                 Matcher match = data_re.matcher(xml.replaceAll("\n", ""));
 
                 if (match.find()) {
-                    observed.Artist = match.group(1);
-                    observed.Album = match.group(2);
-                    observed.Song = match.group(3);
+                    observed.Artist = StringEscapeUtils.unescapeHtml(match.group(1));
+                    observed.Album = StringEscapeUtils.unescapeHtml(match.group(2));
+                    observed.Song = StringEscapeUtils.unescapeHtml(match.group(3));
                     observed.ArtworkUrl = match.group(4);
                     Log.d(TAG, "Got a match! " + observed);
                 } else {
@@ -179,6 +225,7 @@ public class RecoginitionObserverFactory extends RecoginitionObserver {
         }
 
         private void reportResult(Observable observed) {
+            stopRecording();
             // If the recording is still active and we have no match, don't do anything. Otherwise,
             // report the result.
             if (mRecorder == null && observed == null) {
@@ -192,38 +239,45 @@ public class RecoginitionObserverFactory extends RecoginitionObserver {
         }
     }
 
-    /**
-     * Starts the recording and the recorder thread. Results will be posted in the Callback
-     * that was given to the class constructor.
-     */
-    public void startRecording() {
-        mBufferIndex = 0;
-        if (!mRecognitionEnabled) return;
-        try {
-            // Make sure buffer is cleared before recording starts.
-            int bufferSize = SAMPLE_RATE * 11 * 2;
-            mBuffer = new byte[bufferSize];
-            mRecThread = new RecorderThread();
-            mRecorder.startRecording();
-            isRecording = true;
-            mRecThread.start();
-        } catch (IllegalStateException e) {
-            Log.d(TAG, "Cannot start recording for recognition", e);
-            mManager.dispatchRecognitionError();
+    void startRecording() {
+        if (!isRecognitionEnabled){
+            return;
+        }
+        // Only start recording audio if we have internet connectivity.
+        if (mManager.getNetworkStatus() != -1) {
+            new Thread() {
+                @Override
+                public void run() {
+                    Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
+                    try {
+                        try {
+                            // Make sure buffer is cleared before recording starts.
+                            int bufferSize = SAMPLE_RATE * 11 * 2;
+                            mBuffer = new byte[bufferSize];
+                            mRecThread = new RecorderThread();
+                            mRecorder.startRecording();
+                            isRecording = true;
+                            mRecThread.start();
+                        } catch (IllegalStateException e) {
+                            Log.d(TAG, "Cannot start recording for recognition", e);
+                            mManager.dispatchRecognitionError();
+                        }
+                        Thread.currentThread().sleep(mManager.getRecordingMaxTime());
+                        // Stop recording, process audio and post result.
+                        stopRecording();
+                    } catch (InterruptedException e2) {
+                    }
+                }
+            }.start();
         }
     }
 
-    /**
-     * If startRecording was called and still active, the recording system will be stopped and
-     * pending data, if any, will be sent to the API to get a match.
-     */
-    public void stopRecording() {
+    private void stopRecording() {
+        isRecording = false;
         if (mRecorder != null && mRecorder.getState() == AudioRecord.STATE_INITIALIZED && mRecThread != null && mRecThread.isAlive()) {
             try {
                 Log.d(TAG, "Stopping recorder");
-                isRecording = false;
                 mRecorder.stop();
-
                 // Don't forget to release the native resources.
                 mRecorder.release();
                 mRecorder = null;
