3942d26330e9042c8273a0f4cf602393c4c31cac
TheScarastic <warabhishek@gmail.com>
2019-01-02 18:21:52 +0530

    Implement per-app thermal control [2/3]
    Original commit:
    
    services: am: Add intent for active package name [2/2]
    
    Change-Id: Ifc3dcab71a8500307d0157d4b143415dd1d25543
    Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>

---
+++ core/java/com/android/internal/util/custom/thermal/ThermalController.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2019 The PixelExperience Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.android.internal.util.custom.thermal;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+public class ThermalController {
+
+    public static final String SERVICE_PACKAGE = "org.pixelexperience.thermalcontroller";
+    public static final String ACTIVE_PACKAGE_CHANGED_ACTION = "android.intent.action.ACTIVE_PACKAGE_CHANGED";
+    public static final String ACTIVE_PACKAGE_CHANGED_EXTRA = "package_name";
+
+    public static boolean isAvailable(Context context) {
+        final PackageManager pm = context.getPackageManager();
+        try {
+            pm.getPackageInfo(SERVICE_PACKAGE, PackageManager.GET_ACTIVITIES);
+            int enabled = pm.getApplicationEnabledSetting(SERVICE_PACKAGE);
+            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+                    enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+}
