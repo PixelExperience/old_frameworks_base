violet-reborn/android_frameworks_base-1

Improvements in loading notifications with dark mode
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

core/java/android/app/Notification.java
@@ -3299,6 +3299,8 @@ public class Notification implements Parcelable
                         == Configuration.UI_MODE_NIGHT_YES;
             }
 
+            mInNightMode = mInNightMode || res.getBoolean(R.bool.config_useDarkBgNotificationIconTinting);
+
             if (toAdopt == null) {
                 mN = new Notification();
                 if (context.getApplicationInfo().targetSdkVersion < Build.VERSION_CODES.N) {
@@ -5516,10 +5518,8 @@ public class Notification implements Parcelable
                 ensureColors();
                 color = NotificationColorUtil.resolveDefaultColor(mThemeContext, background);
             } else {
-                boolean isDark = mInNightMode || mThemeContext.getResources()
-                        .getBoolean(R.bool.config_useDarkBgNotificationIconTinting);
                 color = NotificationColorUtil.resolveContrastColor(mThemeContext, mN.color,
-                        background, isDark);
+                        background, mInNightMode);
             }
             if (Color.alpha(color) < 255) {
                 // alpha doesn't go well for color filters, so let's blend it manually

PixelExperience/old_frameworks_base

Change-Id: Ieb76ae8f6efce7484e109577f4cc094d67c7c042

core/java/android/app/Notification.java
@@ -3266,6 +3266,8 @@ public class Notification implements Parcelable
             // UI_MODE_NIGHT doesnt seem to be ready, so just listen to config
             mInNightMode = getColorUtil().getNightModeNotification(mContext);
 
+            mInNightMode = mInNightMode || res.getBoolean(R.bool.config_useDarkBgNotificationIconTextTinting);
+
             if (toAdopt == null) {
                 mN = new Notification();
                 if (context.getApplicationInfo().targetSdkVersion < Build.VERSION_CODES.N) {
@@ -5498,10 +5500,8 @@ public class Notification implements Parcelable
                 ensureColors();
                 color = NotificationColorUtil.resolveDefaultColor(mThemeContext, background);
             } else {
-                boolean isDark = mInNightMode || mContext.getResources()
-                        .getBoolean(R.bool.config_useDarkBgNotificationIconTextTinting);
                 color = NotificationColorUtil.resolveContrastColor(mThemeContext, mN.color,
-                        background, isDark);
+                        background, mInNightMode);
             }
             if (Color.alpha(color) < 255) {
                 // alpha doesn't go well for color filters, so let's blend it manually