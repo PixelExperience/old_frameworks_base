2e5f3138aea2745ee43fcf82d5a925d417fd9319
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-08-10 22:06:22 -0300

    WIP - OP Extended screenshot
    Change-Id: I856597056cb56d479a7a2c47dfdfc199da4302b8

---
+++ core/java/com/android/internal/custom/longshot/LongScreenshotConstants.java
@@ -0,0 +1,6 @@
+package com.android.internal.custom.longshot;
+
+public final class LongScreenshotConstants {
+    public static final String PACKAGENAME_LONGSHOT = "com.oneplus.screenshot";
+    public static final String SERVICENAME_LONGSHOT = "com.oneplus.screenshot.LongshotService";
+}
